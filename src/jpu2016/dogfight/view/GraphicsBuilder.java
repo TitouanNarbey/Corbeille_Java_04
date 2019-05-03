9dd0e226545c217a275254721eff44ccc3963acf
benjamin <benjamin@10.169.129.53>
2019-05-03 16:45:35 +0200

    Temporaire Vendredi

---
+++ src/jpu2016/dogfight/view/GraphicsBuilder.java
@@ -0,0 +1,9 @@
+package jpu2016.dogfight.view;
+
+public class GraphicsBuilder {
+	int getGlobalWidth ;
+	int getGlobalHeight ;
+	void buildEmptySky ;
+	void drawMobile(mobile : Imobile, graphics : Graphics, observer : ImageObserver)
+	
+}
